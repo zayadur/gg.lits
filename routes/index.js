f905280f823f19c230e406fa7c77bef6ce870384
Zayadur Khan <zayadur96@gmail.com>
2019-02-27 08:10:00 -0500

    Placeholder landing page

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lost in the Sauce' });
+});
+
+module.exports = router;
